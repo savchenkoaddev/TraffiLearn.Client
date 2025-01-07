354331978624bc8edd3e05452739cd976eeb40ba
Savchenko Artem <p.savchenkoad.back@gmail.com>
2025-01-07 17:00:27 +0200

    TRAF-246: Add register HTML code (#2)

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   imports: [
     MatCheckboxModule,
     FormsModule,
-    RouterLink
+    RouterLink,
+    CommonModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
@@ -16,6 +18,9 @@ import { RouterLink } from '@angular/router';
 export class LoginComponent {
   passwordVisible: boolean = false;
   rememberMe: boolean = false;
+  email: string = '';
+  password: string = '';
+  passwordPattern = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{8,30}$/;
 
   togglePasswordVisibility() {
     this.passwordVisible = !this.passwordVisible;
