e30923b08cf4ce60bb2d51e99ee5ec497ad36097
savchenkoaddev <p.savchenkoad.dev@gmail.com>
2025-01-07 16:24:45 +0200

    Add register html code

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,11 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  username: string = '';
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+  passwordVisible: boolean = false;
+  passwordPattern = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{8,30}$/;
 
+  togglePasswordVisibility() : void {
+    this.passwordVisible = !this.passwordVisible;
+  }
+
+  isConfirmPasswordCorrect() : boolean {
+    return this.password == this.confirmPassword;
+  }
 }
