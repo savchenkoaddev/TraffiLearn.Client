713abbb1111feb4a8fcf1a0f79b4accb07119d5d
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2025-01-09 20:55:49 +0100

    Add toastr package

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideAnimations(),
+    provideToastr({
+      closeButton: true,
+      timeOut: 4000,
+      extendedTimeOut: 2000,
+      easing: 'ease-in',
+      progressBar: true,
+      progressAnimation: 'increasing',
+    }),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient()]
 };
