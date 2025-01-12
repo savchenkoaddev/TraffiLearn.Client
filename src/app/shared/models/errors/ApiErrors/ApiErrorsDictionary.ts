983079f82b536ad5bc24bb5803ef0331cbe19187
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2025-01-12 20:55:07 +0100

    Add new api errors

--- src/app/shared/models/errors/ApiErrors/ApiErrorsDictionary.ts
+++ src/app/shared/models/errors/ApiErrors/ApiErrorsDictionary.ts
@@ -1,6 +1,21 @@
 export class ApiErrorsDictionary {
     static readonly apiErrors: { [key: string]: string } = {
-        'User.AlreadyRegistered': 'Користувач з такою поштою або іменем вже зареєстрований.'
+        // Register errors
+        'User.AlreadyRegistered': 'Користувач з такою поштою або іменем вже зареєстрований.',
+        'Username.Empty': 'Ім\'я користувача не може бути порожнім.',
+        'Username.TooLong': 'Ім\'я користувача занадто довге.',
+        'Username.InvalidFormat': 'Ім\'я користувача має містити тільки літери та цифри.',
+
+        // Login errors
+        'User.NotFound': 'Користувача не знайдено.',
+        'User.InvalidCredentials': 'Неправильні дані для входу.',
+        'User.EmailNotConfirmed': 'Пошту не підтверджено.',
+
+        // Login and Register errors
+        'Email.Empty': 'Адрес пошти не може бути порожнім.',
+        'Email.TooLong': 'Адрес пошти занадто довгий.',
+        'Email.InvalidFormat': 'Неправильний формат пошти.',
+
     }
 
     static getErrorDescription(code: string): string {
